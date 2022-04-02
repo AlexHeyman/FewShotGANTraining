08204cc12317713409e4fd84380ad6c66943da97
AlexHeyman <abheyman23@sbcglobal.net>
2022-04-02 19:07:19 -0400

    Adjusted Generator and associated modules to match original method where appropriate; added input_resolution parameter to Discriminator, but it doesn't do anything yet

--- models.py
+++ models.py
@@ -46,17 +46,19 @@ class Upsampler(nn.Module):
 
 class Generator(nn.Module):
 
-    def __init__(self, use_skips):
+    def __init__(self, use_skips, output_resolution):
+        # output_resolution is assumed to be either 256 or 1024
         super().__init__()
         self.use_skips = use_skips
-        self.conv_transpose = nn.ConvTranspose2d(256, 1024, kernel_size=4,
+        self.output_resolution = output_resolution
+        self.conv_transpose = nn.ConvTranspose2d(256, 2048, kernel_size=4,
                                                  stride=1, padding=0)
-        self.batchnorm = nn.BatchNorm2d(1024)
+        self.batchnorm = nn.BatchNorm2d(2048)
         self.glu = nn.GLU() # cuts number of channels in half
-        self.upsampler_4 = Upsampler(512, 512)
-        self.upsampler_8 = Upsampler(512, 512)
-        self.upsampler_16 = Upsampler(512, 256)
-        self.upsampler_32 = Upsampler(256, 128)
+        self.upsampler_4 = Upsampler(1024, 512)
+        self.upsampler_8 = Upsampler(512, 256)
+        self.upsampler_16 = Upsampler(256, 128)
+        self.upsampler_32 = Upsampler(128, 128)
         self.upsampler_64 = Upsampler(128, 64)
         self.upsampler_128 = Upsampler(64, 32)
         self.upsampler_256 = Upsampler(32, 3)
@@ -65,8 +67,8 @@ class Generator(nn.Module):
         self.nonlinear = nn.Tanh()
         if use_skips:
             self.skip_8_128 = SkipLayerExcitation(64, 512)
-            self.skip_16_256 = SkipLayerExcitation(32, 512)
-            self.skip_32_512 = SkipLayerExcitation(3, 256)
+            self.skip_16_256 = SkipLayerExcitation(32, 256)
+            self.skip_32_512 = SkipLayerExcitation(3, 128)
 
     def forward(self, x):
         x_4 = self.glu(self.batchnorm(self.conv_transpose(x)))
@@ -74,25 +76,34 @@ class Generator(nn.Module):
         x_16 = self.upsampler_8(x_8)
         x_32 = self.upsampler_16(x_16)
         x_64 = self.upsampler_32(x_32)
-        
-        if self.use_skips:
-            x_128 = self.skip_8_128(self.upsampler_64(x_64), x_8)
-            x_256 = self.skip_16_256(self.upsampler_128(x_128), x_16)
-            x_512 = self.skip_32_512(self.upsampler_256(x_256), x_32)
+
+        if self.output_resolution == 256:
+            if self.use_skips:
+                x_128 = self.skip_8_128(self.upsampler_64(x_64), x_8)
+                x_penultimate = self.skip_16_256(
+                    self.upsampler_128(x_128), x_16)
+            else:
+                x_128 = self.upsampler_64(x_64)
+                x_penultimate = self.upsampler_128(x_128)
         else:
-            x_128 = self.upsampler_64(x_64)
-            x_256 = self.upsampler_128(x_128)
-            x_512 = self.upsampler_256(x_256)
-        
-        x_1024 = self.upsampler_512(x_512)
-        return self.nonlinear(self.conv(x_1024))
+            if self.use_skips:
+                x_128 = self.skip_8_128(self.upsampler_64(x_64), x_8)
+                x_256 = self.skip_16_256(self.upsampler_128(x_128), x_16)
+                x_512 = self.skip_32_512(self.upsampler_256(x_256), x_32)
+            else:
+                x_128 = self.upsampler_64(x_64)
+                x_256 = self.upsampler_128(x_128)
+                x_512 = self.upsampler_256(x_256)
+            x_penultimate = self.upsampler_512(x_512)
+
+        return self.nonlinear(self.conv(x_penultimate))
 
 
 class SimpleDecoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, in_channels):
         super().__init__()
-        self.upsampler1 = Upsampler(256, 128)
+        self.upsampler1 = Upsampler(in_channels, 128)
         self.upsampler2 = Upsampler(128, 64)
         self.upsampler3 = Upsampler(64, 32)
         self.upsampler4 = Upsampler(32, 3)
@@ -132,9 +143,11 @@ class ResidualDownsampler(nn.Module):
 
 class Discriminator(nn.Module):
 
-    def __init__(self, use_decoders):
+    def __init__(self, use_decoders, input_resolution):
+        # input_resolution is assumed to be either 256 or 1024
         super().__init__()
         self.use_decoders = use_decoders
+        self.input_resolution = input_resolution
         self.conv1 = nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1)
         self.nonlinear1 = nn.LeakyReLU(negative_slope=0.1)
         self.conv2 = nn.Conv2d(32, 32, kernel_size=4, stride=2, padding=1)
