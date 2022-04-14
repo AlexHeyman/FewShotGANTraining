ad5e6840b123c58da087ed75711b90f9be53fbf3
AlexHeyman <abheyman23@sbcglobal.net>
2022-04-14 15:12:29 -0400

    Forgot the self parameter in the loss modules' forward() methods

--- losses.py
+++ losses.py
@@ -10,7 +10,7 @@ class DiscriminatorReconstructionLoss(torch.nn.Module):
     def __init__(self):
         super().__init__()
     
-    def forward(real_images, decoded_images):
+    def forward(self, real_images, decoded_images):
         return torch.mean(torch.abs(real_images - decoded_images))
 
 
@@ -19,7 +19,7 @@ class DiscriminatorRealFakeLoss(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(real_fake_output_logits_on_real_images,
+    def forward(self, real_fake_output_logits_on_real_images,
                 real_fake_output_logits_on_fake_images):
         real_loss = torch.mean(
             torch.nn.ReLU(1 - real_fake_output_logits_on_real_images))
@@ -35,5 +35,5 @@ class GeneratorLoss(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(real_fake_output_logits_on_fake_images):
+    def forward(self, real_fake_output_logits_on_fake_images):
         return -1 * torch.mean(real_fake_output_logits_on_fake_images)
