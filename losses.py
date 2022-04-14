7f230ce202e298ba2ef78762fb2cc60b6be422f4
AlexHeyman <abheyman23@sbcglobal.net>
2022-04-14 15:11:18 -0400

    Made losses into PyTorch Modules like the preset PyTorch losses are

--- losses.py
+++ losses.py
@@ -4,18 +4,36 @@ Implements the paper's loss functions for training its models.
 
 import torch
 
-def discriminator_reconstruction_loss(real_images, decoded_images):
-    return torch.mean(torch.abs(real_images - decoded_images))
 
-def discriminator_real_fake_loss(real_fake_output_logits_on_real_images,
-                                 real_fake_output_logits_on_fake_images):
-    real_loss = torch.mean(
-        torch.nn.ReLU(1 - real_fake_output_logits_on_real_images))
+class DiscriminatorReconstructionLoss(torch.nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+    
+    def forward(real_images, decoded_images):
+        return torch.mean(torch.abs(real_images - decoded_images))
 
-    fake_loss = torch.mean(
-        torch.nn.ReLU(1 + real_fake_output_logits_on_fake_images))
 
-    return real_loss + fake_loss
+class DiscriminatorRealFakeLoss(torch.nn.Module):
+    
+    def __init__(self):
+        super().__init__()
 
-def generator_loss(real_fake_output_logits_on_fake_images):
-    return -1 * torch.mean(real_fake_output_logits_on_fake_images)
+    def forward(real_fake_output_logits_on_real_images,
+                real_fake_output_logits_on_fake_images):
+        real_loss = torch.mean(
+            torch.nn.ReLU(1 - real_fake_output_logits_on_real_images))
+
+        fake_loss = torch.mean(
+            torch.nn.ReLU(1 + real_fake_output_logits_on_fake_images))
+
+        return real_loss + fake_loss
+
+
+class GeneratorLoss(torch.nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+
+    def forward(real_fake_output_logits_on_fake_images):
+        return -1 * torch.mean(real_fake_output_logits_on_fake_images)
